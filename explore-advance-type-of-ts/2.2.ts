b5ef5e36e77aa7c261471df8433ec5f55f176678
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2026-06-09 23:38:35 +0600

    added interface

---
+++ explore-advance-type-of-ts/2.2.ts
@@ -0,0 +1,25 @@
+//  type allias
+type User1 = {
+  name: string;
+  age: number;
+};
+const user1: User1 = {
+  name: "Persian",
+  age: 100,
+};
+
+// interface
+
+interface User2 {
+  name: string;
+  age: number;
+}
+const user2: User2 = {
+  name: "Persian",
+  age: 100,
+};
+
+//  type script er sate interface er kiso somporko ase,
+// type allias amra saile premitive type er kettro use korte parbe,
+
+type rollNumber = number;
